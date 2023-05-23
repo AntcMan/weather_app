fcfe87c23f6a8d53f5fc5c16750354b958ddb5c8
Anthony Marion <anthonycmarion@gmail.com>
2023-05-23 20:51:30 +1000

    Routes file updated to show search.html.erb as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+get 'weather/search', as: 'search'
+post 'weather/search', to: 'weather#search'
+root 'weather#serch'
 end
